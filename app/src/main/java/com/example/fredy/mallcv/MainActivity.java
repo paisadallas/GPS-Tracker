e6c426659a4b69275aa705a44c1763f774f9f71e
FredyReyes <fredy.reyes.a@gmail.com>
2020-05-23 16:17:38 -0500

    Project GPS
    finish project

---
+++ app/src/main/java/com/example/fredy/mallcv/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.fredy.mallcv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    /*
+
+    public boolean onCreateOptionMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.mymenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    */
+}
